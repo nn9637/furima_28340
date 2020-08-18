0d2c7bc000a7ff23f13127b413ac43be6d42c4de
nn9637 <wd.37nn@gmail.com>
2020-08-18 22:42:36 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,          presence: true
+  validates :email,             presence: true, uniqueness: true, inclusion: { in: ["@"] }
+  validates :password,          presence: true, confirmation: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+  validates :first_name,        presence: true,
+  validates :family_name,       presence: true,
+  validates :first_name_kana,   presence: true,
+  validates :family_name_kana,  presence: true,
+  validates :birthday,          presence: true
+
 end
