dd194abac3e65e22f44569c628fb4221ea46eb81
nn9637 <wd.37nn@gmail.com>
2020-08-22 19:53:34 +0900

    item_imgsテーブルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
+  has_one :item_img
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_22_085944) do
+ActiveRecord::Schema.define(version: 2020_08_22_104445) do
+
+  create_table "item_imgs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "url", null: false
+    t.bigint "item_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_item_imgs_on_item_id"
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -45,5 +53,6 @@ ActiveRecord::Schema.define(version: 2020_08_22_085944) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "item_imgs", "items"
   add_foreign_key "items", "users"
 end
