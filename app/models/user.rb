90a92a5c56fa33578c010ecbcd66d8b44772aed0
nn9637 <wd.37nn@gmail.com>
2020-08-22 17:59:11 +0900

    SyntaxError解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,14 +11,14 @@ class User < ApplicationRecord
     validates :nickname
     validates :email, uniqueness: true
 
-    with_options format: { with: VALID_NAME_REGEX }
+    with_options format: { with: VALID_NAME_REGEX } do
       validates :password, confirmation: true
       validates :password_confirmation
       validates :first_name
       validates :family_name
     end
 
-    with_options format: { with: VALID_PASSWORD_REGEX }
+    with_options format: { with: VALID_PASSWORD_REGEX } do
       validates :first_name_kana
       validates :family_name_kana
     end
