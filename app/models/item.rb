99d9c4c6ef25ae790ec6b6bee5006b411f22c786
nn9637 <wd.37nn@gmail.com>
2020-08-23 18:05:55 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
   has_one :item_img
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 }
 end
--- db/migrate/20200822085944_create_items.rb
+++ db/migrate/20200822085944_create_items.rb
@@ -8,7 +8,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :condition,       null: false
       t.integer :postage_payer,   null: false
       t.integer :preparation_day, null: false
-      t.integer :category,        null: false
+      t.integer :category_id,     null: false
       t.references :user, foreign_key: true
       t.timestamps
     end
