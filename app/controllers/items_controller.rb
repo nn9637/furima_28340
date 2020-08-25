17c514c2d8cb7880aac3895f20fd6b6f5c0dd54e
nn9637 <wd.37nn@gmail.com>
2020-08-25 18:46:30 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,25 +7,10 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-
-  def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
-  end
-
 
   private
 
-  def item_params
-    params.require(:item).permit(:name, user_ids:[])
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 end
