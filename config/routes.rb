3deb5a1b7dac7c83a595339fa58616dea217e218
nn9637 <wd.37nn@gmail.com>
2020-08-25 18:31:35 +0900

    ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'items#index'
+
   resources :users, only: [:new, :create, :destroy]
+  resources :items, only: [:index, :new, :create]
 end
