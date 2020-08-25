75423c3e1277bfbfc18d45090e5765a5a0db1a7b
nn9637 <wd.37nn@gmail.com>
2020-08-25 20:22:21 +0900

    商品出品機能修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,9 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
   with_options presence: true do
     validates :name
     validates :introduction
@@ -9,11 +12,9 @@ class Item < ApplicationRecord
     validates :condition
     validates :postage_payer
     validates :preparation_day
+    validates :category
     validates :price, numericality: { in: 300..9_999_999 }
   end
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
 
   # ジャンルの選択が「--」の時は保存できないようにする
   validates :category_id, numericality: { other_than: 1 }
