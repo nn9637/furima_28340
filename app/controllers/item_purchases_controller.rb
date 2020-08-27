00a5c3a31521a719c4bfdd377d7ab1b991b86300
nn9637 <wd.37nn@gmail.com>
2020-08-27 22:36:18 +0900

    Rubocop

--- app/controllers/item_purchases_controller.rb
+++ app/controllers/item_purchases_controller.rb
@@ -1,8 +1,6 @@
 class ItemPurchasesController < ApplicationController
-
-  def  done
+  def done
     @item_purchases = Item.find(params[:id])
-    @item_purchases.update( item_purchases_id: current_user.id)
+    @item_purchases.update(item_purchases_id: current_user.id)
   end
-
 end
