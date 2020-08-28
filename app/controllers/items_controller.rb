8fd17500a9b258b86e6774966539af6a61ddb281
nn9637 <wd.37nn@gmail.com>
2020-08-28 16:19:51 +0900

    エラーハンドリングの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,8 +24,11 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if item.destroy
+      redirect_to root_path
+    else
+      render :new     
+    end
   end
 
   private
